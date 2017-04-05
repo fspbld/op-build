1515c25048b5075e67222cefee0558c2c0a39585
FSP Build Team <fspbld@us.ibm.com>
2017-04-05 11:04:57 -0500

    reset hostboot-binaries.mk back to 446c2ea (SW376798)

--- openpower/package/hostboot-binaries/hostboot_binaries.mk
+++ openpower/package/hostboot-binaries/hostboot_binaries.mk
@@ -7,9 +7,12 @@
 ###pje 1/06/2017
 ### update for 820.10 H#O#S#TBOOT_BINARIES_VERSION ?= 56532f5370ea875dfafb10a049b5f3ff6882cc08
 # " "  pje 2/22/2017  H#O#S#TBOOT_BINARIES_VERSION ?= 464ab6a961bd062788e73923ff5e10fe4d119d95
-# " " pje 03/15/2017 HOSTBOOT_BINARIES_VERSION ?= 446c2ea03d1359a2c4960c4c590b3886a35ccffe
-
-HOSTBOOT_BINARIES_VERSION ?= cff5784b1ecc60536df08aeaaedc45f237228ca5
+# " " pje 03/15/2017  H#O#S#TBOOT_BINARIES_VERSION ?= 446c2ea03d1359a2c4960c4c590b3886a35ccffe
+# " " pje 04/05/2017 set back to 446c2ea because we 'lost' the fix for sw376798, when we went 
+#                     to cff5784 for 0315n and 0327n
+#                     H#O#S#TBOOT_BINARIES_VERSION ?= cff5784b1ecc60536df08aeaaedc45f237228ca5
+#
+HOSTBOOT_BINARIES_VERSION ?= 446c2ea03d1359a2c4960c4c590b3886a35ccffe
 
 HOSTBOOT_BINARIES_SITE ?= $(call github,open-power,hostboot-binaries,$(HOSTBOOT_BINARIES_VERSION))
 HOSTBOOT_BINARIES_LICENSE = Apache-2.0
